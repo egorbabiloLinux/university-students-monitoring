896694a3d95575ffd0172d53f930e2afff9c7d49
Egor Babilo <egorBabiloLinux@yandex.com>
2025-09-28 00:50:16 +0300

    create httpLogger

--- src/logger.js
+++ src/logger.js
@@ -9,25 +9,4 @@ const logger = pino({
     }
 })
 
-const httpLogger = pinoHttp({
-	logger,
-	serializers: {
-		req: req => ({
-			method: req.method,
-			url: req.url,
-            ...req.extra
-		}),
-		res: res => ({
-			statusCode: res.statusCode,
-			...res.extra
-		}),
-	},
-	customSuccessMessage: (req, res) => {
-		return `msg: ${ req.msgForLogger ?? 'undefined' }`
-	},
-	customErrorMessage: (req, res, err) => {
-		return `msg: ${ req.msgForLogger ?? err.message }`
-	},
-})
-
-module.exports = { logger, httpLogger }
+module.exports = logger
